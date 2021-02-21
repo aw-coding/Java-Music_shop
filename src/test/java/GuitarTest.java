9b4ac29f790fa24224fa35e1c2be681d1b2c441e
Andrew Wood <andrewwood90@hotmail.com>
2021-02-21 16:07:06 +0000

    2 guitar tests passed

--- src/test/java/GuitarTest.java
+++ src/test/java/GuitarTest.java
@@ -19,4 +19,9 @@ public class GuitarTest {
         assertEquals(6, guitar.getNumberOfStrings());
     }
 
+    @Test
+    public void canPlay(){
+        assertEquals("Brrring", guitar.play());
+    }
+
 }
